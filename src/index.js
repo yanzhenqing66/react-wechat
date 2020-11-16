e8b1ec5d64c97269afd68ec3a8e50280a31d18ce
yanzhenqing66 <504767693@qq.com>
2020-11-16 16:12:40 +0800

    路由配置

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button} from 'antd-mobile'
+import App from './App'
 
 
 ReactDOM.render(
-  <Button type="primary">你好</Button>,
+  <App />,
   document.getElementById('root')
 )
